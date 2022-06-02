7e08de9fccd29133c3df421ed6390e9292c512ad
Gwangjin <104111112+jin2424@users.noreply.github.com>
2022-06-02 10:35:22 +0900

    Update app.py

--- hello_world/app.py
+++ hello_world/app.py
@@ -37,7 +37,7 @@ def lambda_handler(event, context):
         "statusCode": 200,
         "body": json.dumps(
             {
-                "message": "hello world! Git Action",
+                "message": "hello world",
                 # "location": ip.text.replace("\n", "")
             }
         ),
